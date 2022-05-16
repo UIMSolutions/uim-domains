e4fcde94ba667b84b70eed4ed77527e0bf966990
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-05-16 07:44:21 +0200

    Adding attributes

--- source/uim/entities/attributes/uuids/entities/file.d
+++ source/uim/entities/attributes/uuids/entities/file.d
@@ -3,22 +3,15 @@ module uim.entities.attributes.uuids.entities.file;
 @safe:
 import uim.entities;
 
-// A unique identifier for entity instances
 class DFileIdAttribute : DEntityIdAttribute {
   mixin(AttributeThis!("FileIdAttribute"));
-
-/*   is.dataFormat.character
-is.dataFormat.big
-is.dataFormat.array
-is.dataFormat.guid
-means.identity.entityId */
 
   override void initialize() {
     super.initialize;
 
     this
       .name("fileId")
-      .registerPath("accounfileIdtid");
+      .registerPath("fileId");
   }  
 }
 mixin(AttributeCalls!("FileIdAttribute"));
--- source/uim/entities/attributes/uuids/entities/package.d
+++ source/uim/entities/attributes/uuids/entities/package.d
@@ -1,12 +1,18 @@
 module uim.entities.attributes.uuids.entityids;
 
+// Main
 public import uim.entities.attributes.uuids.entities.entity;
 
+// Packages
+public import uim.entities.attributes.uuids.entities.slas;
+public import uim.entities.attributes.uuids.entities.users;
+
+// Modules
+public import uim.entities.attributes.uuids.entities.account;
 public import uim.entities.attributes.uuids.entities.businessunit;
+public import uim.entities.attributes.uuids.entities.campaign;
+public import uim.entities.attributes.uuids.entities.contact;
 public import uim.entities.attributes.uuids.entities.currency;
 public import uim.entities.attributes.uuids.entities.file;
-public import uim.entities.attributes.uuids.entities.account;
-public import uim.entities.attributes.uuids.entities.contact;
 public import uim.entities.attributes.uuids.entities.organization;
 
-public import uim.entities.attributes.uuids.entities.users;
