deb19bcc6ffcb332181d046f8d78ead32002cc64
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-11-15 07:12:12 +0100

    Rename namespace to uim.domains

--- source/uim/entities/values/lookups/integers/package.d
+++
@@ -1,5 +0,0 @@
-module uim.entities.values.lookups.integers;
-
-public import uim.entities.values.lookups.integers.boolean;
-public import uim.entities.values.lookups.integers.integer;
-public import uim.entities.values.lookups.integers.string_;
--- source/uim/entities/values/lookups/strings/package.d
+++
@@ -1,5 +0,0 @@
-module uim.entities.values.lookups.strings;
-
-public import uim.entities.values.lookups.strings.boolean;
-public import uim.entities.values.lookups.strings.integer;
-public import uim.entities.values.lookups.strings.string_;
