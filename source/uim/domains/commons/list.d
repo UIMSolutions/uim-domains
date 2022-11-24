5db43ffc132c032bb4385ba1fe87cb1080ea2bb9
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-11-24 19:00:56 +0100

    Updates

--- source/uim/domains/commons/list.d
+++ source/uim/domains/commons/list.d
@@ -13,13 +13,13 @@ class DList {
     return cast(O)this;
   }
 
-  O (this O)add(int index, T item)
+  O (this O)add(int index, T item) {}
 
   // Appends all items in the specified collection to the end of this list, in the order that they are returned by the specified collection's iterator (optional operation).
-  boolean	addAll(Collection<? extends E> c)
+  boolean	addAll(Collection<? extends E> c) {}
 
   // Inserts all of thT items in the specified collection into this list at the specified position (optional operation).
-  boolean	addAll(int index, Collection<? extends E> c)
+  boolean	addAll(int index, Collection<? extends E> c) {}
 
   // Removes all items from this list
   void	clear() {
@@ -28,65 +28,70 @@ class DList {
   }
 
   Returns true if this list contains the specified element.
-  boolean	contains(Object o)
+  boolean	contains(Object o) {}
 
   // Returns true if this list contains all of thT items of the specified collection.
-  boolean	containsAll(Collection<?> c)
+  boolean	containsAll(Collection<?> c) {}
 
   Compares the specified object with this list for equality.
-  boolean	equals(Object o)
+  boolean	equals(Object o) {}
 
   Returns thT item at the specified position in this list.
-  T	get(int index)
+  T	get(int index) {}
 
   Returns hasCode
-  int	hashCode()
+  int	hashCode() {}
 
   // Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain thT item.
   int	indexOf(Object o)
 
   // Returns true if this list contains no elements.
-  boolean	isEmpty()
+  boolean	isEmpty() {}
 
   // Returns an iterator over thT items in this list in proper sequence.
-  Iterator<E>	iterator()
+  Iterator<E>	iterator() {}
 
   // Returns the index of the last occurrence of the specified element in this list, or -1 if this list does not contain thT item.
-  int	lastIndexOf(Object o)
+  int	lastIndexOf(Object o) {}
 
   // Returns a list iterator over thT items in this list (in proper sequence).
-  ListIterator<E>	listIterator()
+  ListIterator<E>	listIterator() {}
 
   // Returns a list iterator over thT items in this list (in proper sequence), starting at the specified position in the list.
-  ListIterator<E>	listIterator(int index)
+  ListIterator<E>	listIterator(int index) {}
 
   // Removes thT item at the specified position in this list (optional operation).
-  T	remove(int index)
+  T	remove(int index) {}
 
   // Removes the first occurrence of the specified element from this list, if it is present (optional operation).
-  boolean	remove(Object o)
+  boolean	remove(Object o) {}
 
   // Removes from this list all of its elements that are contained in the specified collection (optional operation).
-  boolean	removeAll(Collection<?> c)
+  boolean	removeAll(Collection<?> c) {}
 
   // Replaces each element of this list with the result of applying the operator to that element.
   default void	replaceAll(UnaryOperator<E> operator)
 
-  boolean	retainAll(Collection<?> c)
+  boolean	retainAll(Collection<?> c) {}
 
-  E	set(int index, T item)
-Replaces thT item at the specified position in this list with the specified element (optional operation).
-int	size()
-Returns the number of elements in this list.
-default void	sort(Comparator<? super E> c)
-Sorts this list according to the order induced by the specified Comparator.
-default Spliterator<E>	spliterator()
-Creates a Spliterator over thT items in this list.
-List<E>	subList(int fromIndex, int toIndex)
-Returns a view of the portion of this list between the specified fromIndex, inclusive, and toIndex, exclusive.
-Object[]	toArray()
-Returns an array containing all of thT items in this list in proper sequence (from first to last element).
-<T> T[]	toArray(T[] a)
-    void initialize() {}
+  // Replaces thT item at the specified position in this list with the specified element (optional operation).
+  E	set(int index, T item) {}
+
+  // Returns the number of elements in this list.
+  int	size() {}
+
+  // Sorts this list according to the order induced by the specified Comparator.
+  default void	sort(Comparator<? super E> c) {}
+
+  // Creates a Spliterator over thT items in this list.
+  default Spliterator<E>	spliterator() {}
+
+  // Returns a view of the portion of this list between the specified fromIndex, inclusive, and toIndex, exclusive.
+  List<E>	subList(int fromIndex, int toIndex) {}
+
+  // Returns an array containing all of thT items in this list in proper sequence (from first to last element).
+  T[]	toArray() {
+    return _items.dup;
+  }
 }
 auto EventList() { return new DEventList; }
